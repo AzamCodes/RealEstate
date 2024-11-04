1483b28f211b613968ba67034f8884fb2eae81e4
AzamCodes <azam.shaikh9105@gmail.com>
2024-11-04 22:47:29 +0530

    method update

--- api/app.js
+++ api/app.js
@@ -11,7 +11,13 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 app.use(cookieParser());
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
